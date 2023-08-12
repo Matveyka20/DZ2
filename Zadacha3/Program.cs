8f2053e5e2162551aa6d6c0ad24cadf72128a39d
«Matvey <mymayl@inbox.ru>
2023-08-12 20:46:35 +0500

    Zad2

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1 до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 6)
+{
+    System.Console.WriteLine("Это будний день");
+}
+else
+{
+    Console.WriteLine("Это выходной");
+}
